0dffdc4abb0d1c9a68702cdede13366436ff7764
Savvas <karaiskakis.savvas@gmail.com>
2023-06-06 16:02:25 +0100

    additions to be moved

--- Gratistest1.py
+++ Gratistest1.py
@@ -29,6 +29,39 @@ print(result)
 
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+import rpy2.robjects.lib.ggplot2 as ggplot2
+import matplotlib.pyplot as plt
+from io import BytesIO
 
-utils = importr('utils')
-utils.install_packages('lazyeval')
+# Activate pandas conversion for R & Python data frames
+pandas2ri.activate()
+
+# Load required R packages
+base = importr('base')
+gratis = importr('gratis')
+forecast = importr('forecast')
+feasts = importr('feasts')
+
+# Define the R code as a string
+r_code = """
+library(gratis)
+library(ggplot2)
+library(forecast)
+library(feasts)
+set.seed(1)
+ts_obj <- mar_model(seasonal_periods=12) %>%
+  generate(length=120, nseries=2)
+autoplot(ts_obj, y = "value")
+"""
+
+# Run the R code and extract the ggplot2 object
+r_plot = robjects.r(r_code)
+
+# Convert ggplot2 object to an image and display
+r_png = rpy2.robjects.lib.grdevices.render_to_bytesio(r_plot, format="png")
+bytes_io = BytesIO(r_png)
+img = plt.imread(bytes_io)
+plt.imshow(img)
+plt.axis('off')
+plt.show()
--- testts1.py
+++ testts1.py
@@ -1,26 +1,51 @@
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
-from rpy2.robjects.lib.dplyr import DataFrame
-from rpy2.robjects.lib.ggplot2 import ggplot, aes_string
-from rpy2.rinterface_lib.callbacks import logger as r_logger
-import logging
+from rpy2.robjects import pandas2ri, r
+import rpy2.robjects.lib.ggplot2 as ggplot2
+from rpy2.robjects.lib import grdevices
+import matplotlib.pyplot as plt
+from io import BytesIO
+import tempfile
+import os
 
-r_logger.setLevel(logging.ERROR)  # Silence R warning messages
+# Activate pandas conversion for R & Python data frames
+pandas2ri.activate()
 
-# Import the required R packages
-gratis = importr("gratis")
-r_base = importr("base")
-ggplot2 = importr("ggplot2")
+# Load required R packages
+base = importr('base')
+gratis = importr('gratis')
+forecast = importr('forecast')
+feasts = importr('feasts')
 
-# Set the seed value
-r_base.set_seed(1)
+# Define the R code as a string
+r_code = """
+library(gratis)
+library(ggplot2)
+library(forecast)
+library(feasts)
+set.seed(1)
+ts_obj <- mar_model(seasonal_periods=12) %>%
+  generate(length=120, nseries=2)
 
-# Call generate_ts to create the specified time series
-generated_ts = gratis.generate_ts(n=120)
+ts_df <- data.frame(time = time(ts_obj), value = as.vector(ts_obj))
+ggplot(ts_df, aes(x = time, y = value)) + geom_line() + theme_minimal()
+"""
 
-# Create a dataframe and autoplot the generated time series
-plot_data = DataFrame(generated_ts)
-plot = ggplot(plot_data) + ggplot2.geom_line(aes_string(x="Index", y="Value"))
+# Run the R code and extract the ggplot2 object
+r_plot = robjects.r(r_code)
 
-# Display the plot
-plot.show()
+# Save ggplot2 object as a temporary file
+with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp_file:
+    grdevices.png(file=tmp_file.name, width=800, height=600)
+    r.plot(r_plot)
+    grdevices.dev_off()
+
+# Read the saved temporary plot into an image and display
+with open(tmp_file.name, 'rb') as img_f:
+    img = plt.imread(img_f)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+# Remove the temporary file
+os.unlink(tmp_file.name)
